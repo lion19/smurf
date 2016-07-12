2639ee9639e7d72ef9959b040f46b1575c31e08b
Alexander Gallego <gallego.alexx@gmail.com>
2016-07-12 16:24:39 -0400

    remove extra new line on logging

--- src/rpc/rpc_server_stats_printer.cc
+++ src/rpc/rpc_server_stats_printer.cc
@@ -12,7 +12,7 @@ void rpc_server_stats_printer::start() {
   timer_.set_callback([this] {
     stats().then([this](rpc_server_stats stats) {
       std::stringstream ss;
-      ss << "stats: " << stats << std::endl;
+      ss << "stats: " << stats;
       LOG_INFO("{}", ss.str());
     });
   });
